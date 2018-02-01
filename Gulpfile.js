a420d872c3399ff6815bb6607f32dee3a886ec67
darylshy <darylshy@yahoo.com>
2018-01-31 20:03:14 -0500

    converting px to rems; yielding css for older browsers

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     connect = require('gulp-connect'),
+    oldie = require('oldie'),
     clean = require('postcss-clean');
 
 gulp.task('serve', ['css'], function () {
@@ -19,11 +20,19 @@ gulp.task('css', function () {
         clean()
     ];
 
+    var plugins3Oldie = [
+        autoprefixer(),
+        oldie()
+    ];
+
     return gulp.src('./css/*.css')
         .pipe(postcss(plugins))
         .pipe(gulp.dest('./dest'))
         .pipe(postcss(plugins2))
         .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./dest'))
+        .pipe(postcss(plugins3Oldie))
+        .pipe(rename('style.ie8.min.css'))
         .pipe(gulp.dest('./dest'))
         .pipe(connect.reload());
 });
